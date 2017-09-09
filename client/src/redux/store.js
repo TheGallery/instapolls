52ba3c2070756c86f0cd7aa46b25f39a7d313070
Joseph Psychas <iopsychas@gmail.com>
2017-09-09 19:40:14 +0300

    Remove redundant code

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import createHistory from 'history/createBrowserHistory';
+
 import { isAuthenticated } from '../utils/auth';
 
 import reducer from './reducer';
@@ -7,7 +7,5 @@ import reducer from './reducer';
 const defaultState = {
   user: isAuthenticated() ? {} : null
 };
-
-export const history = createHistory();
 
 export default createStore(reducer, defaultState);
