b14573cd64ee5b429d0574447240a73cdd515792
Joseph Psychas <iopsychas@gmail.com>
2017-09-09 19:10:28 +0300

    Add signin page

---
+++ client/src/components/Signin/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {
+  Container,
+  Button,
+  Icon,
+  Header,
+  Divider
+} from 'semantic-ui-react';
+
+import './index.css';
+
+function Signin () {
+  return (
+    <Container className='Signin-root flex-col-center' fluid>
+      <div className='Signin-content'>
+        <Header as='h2' textAlign='center'>Sign in</Header>
+        <Divider />
+        <Button as='a' href='/auth/twitter' color='twitter'>
+          <Icon name='twitter' /> Sign in with Twitter
+        </Button>
+        <Button as='a' href='/auth/github'>
+          <Icon name='github' /> Sign in with Github
+        </Button>
+        <Button as='a' href='/auth/google' color='google plus'>
+          <Icon name='google' /> Sign in with Google
+        </Button>
+      </div>
+    </Container>
+  );
+}
+
+export default Signin;
