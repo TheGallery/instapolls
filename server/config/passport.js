f98b4e464b564a005dc0ee16d0b48e357ebbc7ec
Joseph Psychas <iopsychas@gmail.com>
2017-09-14 19:40:36 +0300

    Update URLs for deployment

--- server/config/passport.js
+++ server/config/passport.js
@@ -8,7 +8,7 @@ const users = require('../controllers/users');
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3001/auth/github/callback'
+  callbackURL: `${process.env.CB_HOST}/auth/github/callback`
 }, function (accessToken, refreshToken, profile, done) {
   users.login(profile, function (err, user) {
     if (err) return done(err);
@@ -21,7 +21,7 @@ passport.use(new GitHubStrategy({
 passport.use(new TwitterStrategy({
   consumerKey: process.env.TWTTR_CONSUMER_KEY,
   consumerSecret: process.env.TWTTR_CONSUMER_SECRET,
-  callbackURL: 'http://localhost:3001/auth/twitter/callback'
+  callbackURL: `${process.env.CB_HOST}/auth/twitter/callback`
 }, function (token, tokenSecret, profile, done) {
   users.login(profile, function (err, user) {
     if (err) return done(err);
@@ -34,7 +34,7 @@ passport.use(new TwitterStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3001/auth/google/callback'
+  callbackURL: `${process.env.CB_HOST}/auth/google/callback`
 }, function (accessToken, refreshToken, profile, done) {
   users.login(profile, function (err, user) {
     if (err) return done(err);
